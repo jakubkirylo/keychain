c87c627b8ad12c1e6ed9287dd2517e6e6ab98bc8
Jakub Wąsowicz-Kiryło <jakub.wasowicz-kirylo@troteclaser.com>
2026-07-23 14:41:22 +0200

    Styling summary component

--- src/app/configurator/pages/summary/summary.component.ts
+++ src/app/configurator/pages/summary/summary.component.ts
@@ -1,9 +1,11 @@
 import { ChangeDetectionStrategy, Component, input } from '@angular/core';
 import { KeychaingConfig } from '../../domain/configurator.interfaces';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-summary',
-  imports: [],
+  imports: [MatCardModule, MatIconModule],
   templateUrl: './summary.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
